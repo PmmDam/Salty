1dc70012c4fe4724980c6fbce7db72b834953e3a
Pablo Muñoz <699328@alu.murciaeduca.es>
2023-06-02 12:26:48 +0200

    Add project files.

---
+++ TFG_Salty/Client/Program.cs
@@ -0,0 +1,20 @@
+//Servicios
+
+
+//Por defecto
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using TFG_Salty.Client;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
